9f85b89ba5e09dd63e0a4f62448192cb1c7e3ca4
Jordan <jkimys2@gmail.com>
2023-12-02 11:21:53 -0500

    script update

--- assets/js/script.js
+++ assets/js/script.js
@@ -16,58 +16,53 @@ function generatePassword() {
 
 
   // Variables that contain the characters that will make up the random password
-  var passwordLength
+  // var passwordLength
   var specialCharactersChoice = "! # $ % & ' ( ) * + , - / : ; < = > ? [ | \ ] ^ _ { } | ~ `";
-  var numberChoice = "0 1 2 3 4 5 6 7 8 9";
+  var numericChoice = "0 1 2 3 4 5 6 7 8 9";
   var lowecaseChoice = "a b c d e f g h i j k l m n o p q r s t u v w x y z";
   var uppercaseChoice =  "A B C D E F G H I J K L M N O P Q R S T U V W X Y Z";
   
-  // Prompts that will alert the user to choose the 
+  // Prompt that will allow the user to choose a password length
   var passwordLength = prompt("How many characters would you like your password to contain?");
   
-  // when user puts in a number that is not between 8 and 128, it will alert the user to change their value to match the criteria
+
+   // if statement to set the parameters of the password length
   if (passwordLength < 8 || passwordLength > 128) {
-    alert("You must choose between 8 and 128 characters. Please try again.")
+    alert("You must choose between 8 and 128 characters! \nPlease try again.")
     var passwordLength = prompt("How many characters would you like your password to contain?");
-    console.log(passwordLength: )
-    }
-
-    // allows user to choose if they want to include special characters
-    var specialCharactersChoice = confirm("Please select OK to include special characters!")
-    var specialCharactersChoice = " ! @ # $ ^ & * ( ) /"
-
-    // if statement to include special characters in password
-    if (specialCharactersChoice)
-
-    // allows user to include numbers in password
-    var numericChoice = confirm("Please select OK to include numbers!")
-
-    // if statement to include numbers
-
-
-    // allows user to include lowercase letters
-    var lowercaseChoice = confirm("Please select OK to include lowercase characters!")
-   
-    // if state to include lowercase letters
-
-
-    // allows user to include uppercase letters
-    var uppercaseChoice = confirm("Please select OK to include uppercase characters!")
-    
-    // if statement to include uppercase letters
-    if (uppercase) {
-      // logic that includes the upper case letters
-      // create array that holds the potential characters that will make up the PW
-     
-    }
-// push the values based on potiential characters
-var potentialCharacters = 
-// create an arrau variable that holds the uipper characters
+    console.log(passwordLength)
+   }
+  
+  // prompts that will allow the user to choose the rest of the criteria of their password
+  var specialCharactersChoice = confirm("Please select OK to include special characters!");
+  var numericChoice = confirm("Please select OK to include numbers!");
+  var lowercaseChoice = confirm("Please select OK to include lowercase characters!");
+  var uppercaseChoice = confirm("Please select OK to include uppercase characters!");
 
 }
-potentialcharacters.push(uppercase)
-// time to generate passwords
-var length = 
+  
+
+//     // if statements to include the prompts that are chosen by the user
+//     if (specialCharactersChoice)
+
+    
+   
+//     // if state to include lowercase letters
+    
+//     // if statement to include uppercase letters
+//     if (uppercase) {
+//       // logic that includes the upper case letters
+//       // create array that holds the potential characters that will make up the PW
+     
+//     }
+// // push the values based on potiential characters
+// var potentialCharacters = 
+// // create an arrau variable that holds the uipper characters
+
+// }
+// potentialcharacters.push(uppercase)
+// // time to generate passwords
+// var length = 
 
 
 // once i get my potcharact with all of the potential characters based on user choice, I need to iterate through the array and choose a random character from the potential characters arrays
