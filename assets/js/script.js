78a154cda600493eb8fc29d833d0b03b70b3e16f
Jordan <jkimys2@gmail.com>
2023-12-02 11:03:59 -0500

    script update

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,14 +14,22 @@ function writePassword() {
 // create a function that will return the final password
 function generatePassword() {
 
-    // when user clicks generate password button, the page will prompt user to put in a number for length of password
-    var passwordLength = prompt("How many characters would you like your password to contain?");
 
-    // when user puts in a number that is not between 8 and 128, it will alert the user to change their value to match the criteria
-    if (passwordLength < 8 || passwordLength > 128) {
-        alert("You must choose between 8 and 128 characters. Please try again.")
-        var passwordLength = prompt("How many characters would you like your password to contain?");
-        console.log(passwordLength: )
+  // Variables that contain the characters that will make up the random password
+  var passwordLength
+  var specialCharactersChoice = "! # $ % & ' ( ) * + , - / : ; < = > ? [ | \ ] ^ _ { } | ~ `";
+  var numberChoice = "0 1 2 3 4 5 6 7 8 9";
+  var lowecaseChoice = "a b c d e f g h i j k l m n o p q r s t u v w x y z";
+  var uppercaseChoice =  "A B C D E F G H I J K L M N O P Q R S T U V W X Y Z";
+  
+  // Prompts that will alert the user to choose the 
+  var passwordLength = prompt("How many characters would you like your password to contain?");
+  
+  // when user puts in a number that is not between 8 and 128, it will alert the user to change their value to match the criteria
+  if (passwordLength < 8 || passwordLength > 128) {
+    alert("You must choose between 8 and 128 characters. Please try again.")
+    var passwordLength = prompt("How many characters would you like your password to contain?");
+    console.log(passwordLength: )
     }
 
     // allows user to choose if they want to include special characters
@@ -33,22 +41,19 @@ function generatePassword() {
 
     // allows user to include numbers in password
     var numericChoice = confirm("Please select OK to include numbers!")
-    var numericChoice = "0 1 2 3 4 5 6 7 8 9"
 
     // if statement to include numbers
 
 
     // allows user to include lowercase letters
     var lowercaseChoice = confirm("Please select OK to include lowercase characters!")
-    var lowercaseChoice = " a b c d e f g h i j k l m n o p q r s t u v w x y z"
-
+   
     // if state to include lowercase letters
 
 
     // allows user to include uppercase letters
     var uppercaseChoice = confirm("Please select OK to include uppercase characters!")
-    var uppercaseChoice = "A B C D E F G H I J K L M N O P Q R S T U V W X Y Z"
-  
+    
     // if statement to include uppercase letters
     if (uppercase) {
       // logic that includes the upper case letters
@@ -69,6 +74,9 @@ var length =
 
 // iterate thorugh array and choose one randome charact based on chosen link, keep in mind you want each character type represented
 // if the lenght is 10, you need to iterate through it 10 times
+
+
+
 
 
 
